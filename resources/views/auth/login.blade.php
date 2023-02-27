12fdcff6f42798068c680819bbe01d9fb3e06d94
elhamsyahrianputra <72007074+elhamsyahrianputra@users.noreply.github.com>
2023-02-27 19:06:41 +0700

    fix: auth views

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,6 +8,8 @@
     <title>Baswara | Login</title>
     <link rel="stylesheet" href="mazer/css/main/app.css">
     <link rel="stylesheet" href="mazer/css/pages/auth.css">
+    <link rel="stylesheet" href="dist/css/style.css">
+
     <link rel="stylesheet" href="mazer/extensions/@fortawesome/fontawesome-free/css/all.min.css">
     <link rel="shortcut icon" href="mazer/images/logo/favicon.svg" type="image/x-icon">
     <link rel="shortcut icon" href="mazer/images/logo/favicon.png" type="image/png">
@@ -19,7 +21,7 @@
             <div class="col-lg-5 col-12">
                 <div id="auth-left">
                     <div class="auth-logo">
-                        <a href="/"><img src="mazer/images/logo/logo.svg" alt="Logo"></a>
+                        <a href="/"><img src="{{ asset('dist/img/logo/full_logo.png') }}" alt="Baswara Logo" style="height: 100px"></a>
                     </div>
                     <h1 class="auth-title">Log in.</h1>
                     <p class="auth-subtitle mb-5">Log in with your data that you entered during registration.</p>
@@ -44,7 +46,9 @@
                                 Keep me logged in
                             </label>
                         </div>
-                        <button class="btn btn-primary btn-block btn-lg shadow-lg mt-5">Log in</button>
+                        <button class="btn btn--gradient btn-block btn-lg shadow-lg mt-5">
+                            <span class="text">Log in</span>
+                        </button>
                     </form>
                     <div class="text-center mt-5 text-lg fs-4">
                         <p class="text-gray-600">Don't have an account? <a href="/registration"
